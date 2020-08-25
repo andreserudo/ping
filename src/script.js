f1d266d9d4670fafd5ce985f454440a5428445b9
Andre Serudo <andreserudo@gmail.com>
2020-08-24 22:41:10 -0400

    adding style for invalid email

--- src/script.js
+++ src/script.js
@@ -6,10 +6,12 @@ function emailIsValid (email) {
 function validEmail(){
   const email = document.getElementById('email').value;
   
-  if(emailIsValid(email)){
-    alert('email válido!')
-  }else{
-    alert('email inválido!')
+  if(emailIsValid(email)){    
+    document.getElementById('rejectedEmail').style.display = "none";
+    document.getElementById('email').style.borderColor = "#e7ecff";
+  }else{    
+    document.getElementById('rejectedEmail').style.display = "block";    
+    document.getElementById('email').style.borderColor = "#f9b0c2";
   }
   
 }
